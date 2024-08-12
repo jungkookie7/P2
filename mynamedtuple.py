9dce4fe943975b61c547360517bd2180c7032fce
jungkookie7 <sinnj@uci.edu>
2024-08-12 04:56:30 -0700

    mynamedtuple.py

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -2,67 +2,57 @@ import keyword
 
 def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
     # testing type_name
-    if type(type_name) != str:  # test if a string
+    if type(type_name) != str: 
         raise SyntaxError
-    elif not type_name[0].isalpha():  # test if first element is letter
+    elif not type_name[0].isalpha():  
         raise SyntaxError
-    elif keyword.iskeyword(type_name):  # test if conflicts with keywords
+    elif keyword.iskeyword(type_name):  
         raise SyntaxError
-    else:  # passes all tests
-        pass
     
     # testing field_names
-    if type(field_names) == str:  # tests if str, if so splits
+    if type(field_names) == str: 
         field_names = field_names.replace(',', ' ').split()
 
-    if type(field_names) != list or len(field_names) == 0:  # tests if field_name is NOT a list or empty
+    if type(field_names) != list or len(field_names) == 0:  
         raise SyntaxError
 
     for i in field_names:
-        if not i[0].isalpha():  # tests if first element is letter
+        if not i[0].isalpha():  
             raise SyntaxError
-        elif keyword.iskeyword(i):  # tests if conflicts with keywords
+        elif keyword.iskeyword(i):  
             raise SyntaxError
-        else:  # passes all tests
-            pass
 
     # testing for duplicates
-    a = []
+    lst = []
     seen = {}  
-
-    for name in field_names:
-        if name not in seen:
-            a.append(name)
-            seen[name] = True  
-    field_names = a
+    for i in field_names:
+        if i not in seen:
+            lst.append(i)
+            seen[i] = True  
+    field_names = lst
 
     # testing defaults
-    if type(defaults) != dict:  # tests if default is a dict
+    if type(defaults) != dict:  
         raise SyntaxError
     
     for i in defaults:
-        if i not in field_names:  # tests if element is in field_name
+        if i not in field_names:  
             raise SyntaxError
-        else:  # passes all tests
-            pass
     
     # new line and tab
-    new = ('\n')
-    tab = ('    ')
+    new = '\n'
+    tab = '    '
 
-    # class code
-    big_string = (f'class {type_name}:')
+    # large string for the class
+    big_string = f'class {type_name}:'
     big_string += new
-    big_string += (f'{tab}_fields = {field_names}')
+    big_string += f'{tab}_fields = {field_names}'
     big_string += new
-    big_string += (f'{tab}_mutable = {mutable}')
+    big_string += f'{tab}_mutable = {mutable}'
     big_string += new
 
     # __init__
-    init_params = ''
-    for name in field_names:
-        init_params += f"{name}={defaults.get(name, 'None')}, "
-    init_params = init_params.rstrip(', ')  
+    init_params = ', '.join(f"{name}={defaults.get(name, 'None')}" for name in field_names)
     big_string += f"{tab}def __init__(self, {init_params}):{new}"
     
     for name in field_names:
@@ -71,67 +61,60 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
 
     # __repr__ method
     big_string += f"{tab}def __repr__(self):{new}"
-    repr_fields = ''
-    for name in field_names:
-        repr_fields += f"{name}={{self.{name}!r}},"
-    repr_fields = repr_fields.rstrip(', ')  
+    repr_fields = ', '.join(f"{name}={{self.{name}!r}}" for name in field_names)
     big_string += f"{tab*2}return f'{type_name}({repr_fields})'{new}{new}"
 
     # Accessor methods
     for name in field_names:
-        big_string += f"    def get_{name}(self):\n"
-        big_string += f"        return self.{name}\n\n"
+        big_string += f"{tab}def get_{name}(self):{new}"
+        big_string += f"{tab*2}return self.{name}{new}{new}"
 
     # __getitem__ method
-    big_string += "    def __getitem__(self, index):\n"
-    big_string += f"        return getattr(self, self._fields[index])\n\n"
+    big_string += f"{tab}def __getitem__(self, index):{new}"
+    big_string += f"{tab*2}return getattr(self, self._fields[index]){new}{new}"
 
     # __eq__ method
-    big_string += "    def __eq__(self, other):\n"
-    big_string += "        if not isinstance(other, self.__class__):\n"
-    big_string += "            return NotImplemented\n"
-    big_string += "        return all(getattr(self, name) == getattr(other, name) for name in self._fields)\n\n"
+    big_string += f"{tab}def __eq__(self, other):{new}"
+    big_string += f"{tab*2}if not isinstance(other, self.__class__):{new}"
+    big_string += f"{tab*3}return NotImplemented{new}"
+    big_string += f"{tab*2}return all(getattr(self, name) == getattr(other, name) for name in self._fields){new}{new}"
 
     # asdict method
-    big_string += "    def asdict(self):\n"
-    big_string += "        return {name: getattr(self, name) for name in self._fields}\n\n"
-
-    # asdict method
-    big_string += "    def asdict(self):\n"
-    big_string += "        return {name: getattr(self, name) for name in self._fields}\n\n"
+    big_string += f"{tab}def asdict(self):{new}"
+    big_string += f"{tab*2}return {{name: getattr(self, name) for name in self._fields}}{new}{new}"
 
     # make method
-    big_string += "    @classmethod\n"
-    big_string += "    def make(cls, iterable):\n"
-    big_string += "        return cls(*iterable)\n\n"
+    big_string += f"{tab}@classmethod{new}"
+    big_string += f"{tab}def make(cls, iterable):{new}"
+    big_string += f"{tab*2}return cls(*iterable){new}{new}"
 
     # _make method
-    big_string += "    @classmethod\n"
-    big_string += "    def _make(cls, iterable):\n"
-    big_string += "        return cls(*iterable)\n\n"
+    big_string += f"{tab}@classmethod{new}"
+    big_string += f"{tab}def _make(cls, iterable):{new}"
+    big_string += f"{tab*2}return cls(*iterable){new}{new}"
 
     # replace method
-    big_string += "    def replace(self, **kwargs):\n"
-    big_string += "        if self._mutable:\n"
-    big_string += "            for name, value in kwargs.items():\n"
-    big_string += "                if name in self._fields:\n"
-    big_string += "                    object.__setattr__(self, name, value)\n"
-    big_string += "            return None\n"
-    big_string += "        else:\n"
-    big_string += "            new_values = {name: kwargs.get(name, getattr(self, name)) for name in self._fields}\n"
-    big_string += "            return self.__class__(**new_values)\n\n"
+    big_string += f"{tab}def replace(self, **kwargs):{new}"
+    big_string += f"{tab*2}if self._mutable:{new}"
+    big_string += f"{tab*3}for name, value in kwargs.items():{new}"
+    big_string += f"{tab*4}if name in self._fields:{new}"
+    big_string += f"{tab*5}object.__setattr__(self, name, value){new}"
+    big_string += f"{tab*2}return None{new}"
+    big_string += f"{tab*2}else:{new}"
+    big_string += f"{tab*3}new_values = {{name: kwargs.get(name, getattr(self, name)) for name in self._fields}}{new}"
+    big_string += f"{tab*3}return self.__class__(**new_values){new}{new}"
 
     # _replace method
-    big_string += "    def _replace(self, **kwargs):\n"
-    big_string += "        new_values = {name: kwargs.get(name, getattr(self, name)) for name in self._fields}\n"
-    big_string += "        return self.__class__(**new_values)\n\n"
+    big_string += f"{tab}def _replace(self, **kwargs):{new}"
+    big_string += f"{tab*2}new_values = {{name: kwargs.get(name, getattr(self, name)) for name in self._fields}}{new}"
+    big_string += f"{tab*2}return self.__class__(**new_values){new}{new}"
 
     # __setattr__ method
-    big_string += "    def __setattr__(self, name, value):\n"
-    big_string += "        if self._mutable or name not in self._fields or not hasattr(self, name):\n"
-    big_string += "            object.__setattr__(self, name, value)\n"
-    big_string += "        else:\n"
-    big_string += "            raise AttributeError(f'Cannot modify {name} in immutable {self.__class__.__name__}')\n"
+    big_string += f"{tab}def __setattr__(self, name, value):{new}"
+    big_string += f"{tab*2}if self._mutable or name not in self._fields or not hasattr(self, name):{new}"
+    big_string += f"{tab*3}object.__setattr__(self, name, value){new}"
+    big_string += f"{tab*2}else:{new}"
+    big_string += f"{tab*3}raise AttributeError(f'Cannot modify {{name}} in immutable {{self.__class__.__name__}}'){new}"
 
     # Execute the generated class code
     namespace = {}
