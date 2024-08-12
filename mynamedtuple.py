318a5f74c2acc078d2148bcb2b778a6e8beaf409
jungkookie7 <sinnj@uci.edu>
2024-08-12 06:24:35 -0700

    mynamedtuple.py

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -97,10 +97,7 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
     big_string += (f'{tab}def __eq__(self, other):{new}')
     big_string += (f'{tab*2}if not isinstance(other, self.__class__):{new}')
     big_string += (f'{tab*3}return NotImplemented{new}')
-    big_string += (f'{tab*2}for name in self._fields:{new}')
-    big_string += (f'{tab*3}if getattr(self, name) != getattr(other, name):{new}')
-    big_string += (f'{tab*4}return False{new}')
-    big_string += (f'{tab*2}return True{new*2}')
+    big_string += (f'{tab*2}return all(getattr(self, name) == getattr(other, name) for name in self._fields){new}')
     #print(big_string) *TEST MIDWAY
 
     # asdict 
