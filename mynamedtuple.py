919db0cdb3f379e5e15d016335d0f4cf1364d4d8
jungkookie7 <sinnj@uci.edu>
2024-08-11 22:32:04 -0700

    mynamedtuple.py

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -45,39 +45,72 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
             raise SyntaxError
         else: #passes all tests
             pass
-    
-class coordinate:
-    _fields = ['x', 'y']
-    _mutable = False
 
-    def __init__(self, type_name, field_names, mutable = False, defaults={}):
-        self.type_name = type_name
-        self._fields = field_names.split()
-        self._mutable = mutable
-        self.defaults = defaults
-        
-        # Dynamically create the __init__ method
-        init_code = "def __init__(self, " + ", ".join(
-            f"{field}={defaults.get(field, '')!r}" if field in defaults else field
-            for field in self._fields
-        ) + "):\n"
-        
-        # Initialize fields
-        for field in self._fields:
-            init_code += f"    self.{field} = {field}\n"
-        
-        # Execute the dynamically created __init__ method
-        exec(init_code, globals(), locals())
-        self.__init__ = locals()['__init__']
+    #class code
+    class_code = f"class {type_name}:\n"
+    class_code += f"    _fields = {field_names}\n"
+    class_code += f"    _mutable = {mutable}\n\n"
 
-    def __repr__(self):
-        fields_repr = ", ".join(f"{field}={repr(getattr(self, field))}" for field in self._fields)
-        return f"{self.type_name}({fields_repr})"
-    
-    def get_x(self):
-        return self.x
-    def get_y(self):
-        return self.y
+    # __init__ method
+    init_params = ', '.join([f"{name}={defaults.get(name, 'None')}" for name in field_names])
+    class_code += f"    def __init__(self, {init_params}):\n"
+    for name in field_names:
+        class_code += f"        self.{name} = {name}\n"
+    class_code += "\n"
+
+    # __repr__ method
+    class_code += "    def __repr__(self):\n"
+    repr_str = f"{type_name}(" + ', '.join([f"{name}={{self.{name}!r}}" for name in field_names]) + ")"
+    class_code += f"        return f'{repr_str}'\n\n"
+
+    # Accessor methods
+    for name in field_names:
+        class_code += f"    def get_{name}(self):\n"
+        class_code += f"        return self.{name}\n\n"
+
+    # __getitem__ method
+    class_code += "    def __getitem__(self, index):\n"
+    class_code += f"        return getattr(self, self._fields[index])\n\n"
+
+    # __eq__ method
+    class_code += "    def __eq__(self, other):\n"
+    class_code += "        if not isinstance(other, self.__class__):\n"
+    class_code += "            return NotImplemented\n"
+    class_code += "        return all(getattr(self, name) == getattr(other, name) for name in self._fields)\n\n"
+
+    # asdict method
+    class_code += "    def asdict(self):\n"
+    class_code += "        return {name: getattr(self, name) for name in self._fields}\n\n"
+
+    # make method
+    class_code += "    @classmethod\n"
+    class_code += "    def make(cls, iterable):\n"
+    class_code += "        return cls(*iterable)\n\n"
+
+    # replace method
+    class_code += "    def replace(self, **kwargs):\n"
+    class_code += "        if self._mutable:\n"
+    class_code += "            for name, value in kwargs.items():\n"
+    class_code += "                setattr(self, name, value)\n"
+    class_code += "            return None\n"
+    class_code += "        else:\n"
+    class_code += "            new_values = {name: kwargs.get(name, getattr(self, name)) for name in self._fields}\n"
+    class_code += "            return self.__class__(**new_values)\n\n"
+
+    # __setattr__ method
+    class_code += "    def __setattr__(self, name, value):\n"
+    class_code += "        if not self._mutable and name in self._fields and hasattr(self, name):\n"
+    class_code += "            raise AttributeError(f'Cannot modify {name} in immutable {self.__class__.__name__}')\n"
+    class_code += "        super().__setattr__(name, value)\n"
+
+    # Execute the generated class code
+    namespace = {}
+    exec(class_code, namespace)
+    return namespace[type_name]
 
 #TESTERS
 coordinate = mynamedtuple('coordinate', ['x','y'], mutable=False) #testing tuple number 1
+coordinate = mynamedtuple('coordinate', 'x y')
+p = coordinate(0, 0)
+print(p) # coordinate(x=0,y=0)
+print(p.asdict()) #{’x’: 0, ’y’: 0}
