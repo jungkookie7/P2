c0165bd01eba8bfd1b57029ebe6964a4cbcb8abe
jungkookie7 <sinnj@uci.edu>
2024-08-11 20:05:51 -0700

    DictTuple.py

--- DictTuple.py
+++ DictTuple.py
@@ -1,1 +1,19 @@
-pass
+from collections import namedtuple
+import mynamedtuple
+
+class DictTuple:
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __bool__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+coordinate = mynamedtuple('coordinate', 'x y')
+d = DictTuple({'c1': coordinate(1, 2)}, {'c1': coordinate(3, 4)})
+print(d)
