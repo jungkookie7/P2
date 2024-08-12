1ac3ddca1501a01756db047b8e99bd5c08c8354f
jungkookie7 <sinnj@uci.edu>
2024-08-12 05:58:37 -0700

    mynamedtuple.py

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -60,11 +60,11 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
     #print(big_string) *TEST MIDWAY
 
     # __init__
-    init_params = ''
-    for name in field_names:
-        init_params += f"{name}={defaults.get(name, 'None')}, "
-    init_params = init_params.rstrip(', ')  
-    big_string += f"{tab}def __init__(self, {init_params}):{new}"
+    parameters = ''
+    for i in field_names:
+        parameters += f"{i}={defaults.get(i, 'None')}, "
+    parameters = parameters.rstrip(', ')  
+    big_string += f"{tab}def __init__(self, {paremeters}):{new}"
     
     for name in field_names:
         big_string += f"{tab*2}self.{name} = {name}{new}"
@@ -100,7 +100,7 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults={}):
     #print(big_string) *TEST MIDWAY
 
     # asdict 
-    big_string += (f'{tab}def _asdict(self):{new}')
+    big_string += (f'{tab}def asdict(self):{new}')
     big_string += (f"{tab*2}return self.asdict(){new*2}")
     #print(big_string) *TEST MIDWAY
 
